9740f70ff2a5e2b35f17ab9ce890aa34dc636ab2
Sean Davis <sdavis2702@gmail.com>
2022-05-16 11:23:56 -0500

    combine relevant strings into one #4

--- includes/display/class-sccfd-display-course.php
+++ includes/display/class-sccfd-display-course.php
@@ -26,17 +26,22 @@ class SCCFD_Display_Course {
 	public function scc_front_display( $content ) {
 		global $post;
 
-		// set leading text and filter it
-		$course_leading_text = apply_filters( 'course_leading_text', __( 'This post is part of the', 'scc_front_display' ) );
-		$course_trailing_text = apply_filters( 'course_trailing_text', 'course.' );
+		// get the name of the course if it exists
+		$course_info = get_the_term_list( $post->ID, 'course' );
 
-		// what's the name of the course?
-		$course_info = get_the_term_list( $post->ID, 'course', $course_leading_text . ' ', ', ', ' ' . $course_trailing_text );
+		// bail if there is no course
+		if ( ! $course_info ) {
+			return $content;
+		}
 
-		// only display the course output if it exists and it's the
-		// blog home, archive, or search results
-		if ( $course_info && ( is_home() || is_archive() || is_search() ) ) :
-			$content = '<p class="scc-front-display">' . $course_info . '</p> ' . $content;
+		// get the front display output
+		$course_output = apply_filters( 'scc_front_display_output',
+			sprintf( __( 'This post is part of the %s course.', 'scc_front_display' ), $course_info )
+		);
+
+		// only display the output on blog home, archive, and search results
+		if ( ( is_home() || is_archive() || is_search() ) ) :
+			$content = '<p class="scc-front-display">' . $course_output . '</p> ' . $content;
 		endif;
 
 		return $content;
